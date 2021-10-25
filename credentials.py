3cad3d7da34fe2be621ada38a390940163962130
mohamedissack <mohamed.abdi@student.moringashool.com>
2021-10-25 06:43:32 +0300

    add method

--- credentials.py
+++ credentials.py
@@ -4,8 +4,18 @@ import string  #import string constants
 import pyperclip
 
 class Credentials:
+    
     '''
     Class to create  account credentials, generate new passwords and save user information
     '''
     
     credential_list = []
+    def __init__(self,user_name,account_name,password):
+        '''
+        Method to define the properties for each user object.
+        '''
+        self.user_name = user_name
+        self.account_name = account_name
+        self.password = password
+    
+    
