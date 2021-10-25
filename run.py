29636dbf7bd772c6dbc150d44585768659372369
mohamedissack <mohamed.abdi@student.moringashool.com>
2021-10-25 07:24:37 +0300

    update run.py file

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3.8
 import pyperclip 
 from user import User
-from credentials import Credentials
 import random #import random variable generator
 import string  #import string constants
+
 
 def create_user(user_name,password):
     '''
@@ -12,18 +12,20 @@ def create_user(user_name,password):
     new_user = User(user_name,password)
     return new_user
 
-    def save_user(user):
+def save_user(user):
     '''
     Function to save new user account
     '''
     user.save_user()
-    def verify_user(user_name,password):
+    
+def verify_user(user_name,password):
     '''
     Function that verifies the existing user
     
     '''
     check_user = Credentials.check_user(user_name,password)
     return check_user
+
 
 def create_credential(user_name,account_name,password):
     '''
@@ -32,13 +34,13 @@ def create_credential(user_name,account_name,password):
     new_credential = Credentials(user_name,account_name,password)
     return new_credential
 
-    def check_existing_account(account_name):
+def check_existing_account(account_name):
     '''
     Function that checks if credential exists
     '''
     return Credentials.find_by_account_name(account_name)
 
-    def generate_password(length = 10):
+def generate_password(length = 10):
     '''
     Function that generates password automatically
     '''
@@ -46,31 +48,31 @@ def create_credential(user_name,account_name,password):
     password_generated = ''.join(random.choice(letters) for i in range(length))
     return password_generated
 
-    def save_credentials(credential):
+def save_credentials(credential):
     '''
     Function that saves new credential
     '''
     Credentials.save_credentials(credential)
-
-    def delete_credential(credential):
+    
+def delete_credential(credential):
     '''
     Function that deletes a credential
     '''
     Credentials.delete_credentials(credential)
-
-    def find_credential(account_name):
+    
+def find_credential(account_name):
     '''
     Function that credential by account_name and returns credential
     '''
     return Credentials.find_by_account_name(account_name)
 
-    def display_credential(credentials):
+def display_credential(credentials):
     '''
     Function that displays saved credentials
     '''
     return Credentials.display_credentials()
 
-    def copy_credentials(credentials):
+def copy_credentials(credentials):
     '''
     Function that displays saved credentials
     '''
@@ -227,4 +229,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-
