c265334103d120784aa43ff6c324d57ad78e6495
mohamedissack <mohamed.abdi@student.moringashool.com>
2021-10-25 06:59:02 +0300

    add class user

--- user.py
+++ user.py
@@ -1,1 +1,19 @@
 import pyperclip
+
+class User:
+    '''
+    class that allows user to generate user account
+    '''
+    
+    user_list = []
+    
+    def __init__(self,user_name,password):
+        self.user_name = user_name
+        self.password = password
+        
+    def save_user(self):
+        '''
+        save_user method that saves new user object
+        '''
+        User.user_list.append(self)
+
