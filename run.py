8071ab9ca70da011a56cce27fc9b70782dc9da9c
mohamedissack <mohamed.abdi@student.moringashool.com>
2021-10-25 07:03:30 +0300

    add imports

---
+++ run.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3.8
+import pyperclip 
+from user import User
+from credentials import Credentials
+import random #import random variable generator
+import string  #import string constants
