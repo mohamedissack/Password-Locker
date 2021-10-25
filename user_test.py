d178e998439d50ebd5c40ac8ed4e276725353bfc
mohamedissack <mohamed.abdi@student.moringashool.com>
2021-10-25 06:53:59 +0300

    add class test user

--- user_test.py
+++ user_test.py
@@ -1,2 +1,23 @@
 import unittest # Importing the unittest module
 from user import User #importing user class
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class behaviors.
+    '''
+    
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        
+        self.new_user = User('Salah', '123456')
+        
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.user_name,"Salah")
+        self.assertEqual(self.new_user.password,"123456")
+    
