bccbc5726f8ab4945587a0c3e9a1d2f1267b488b
mohamedissack <mohamed.abdi@student.moringashool.com>
2021-10-25 06:34:12 +0300

    adddef copy acc name  credentials

--- credencials_test.py
+++ credencials_test.py
@@ -84,3 +84,15 @@ def test_display_all_credentials(self):
             credential_exists = Credentials.credential_exist("Twitter")
 
             self.assertTrue(credential_exists)
+
+            def test_copy_account_name(self):
+        '''
+        Test to confirm that we are copying the account_name from a found credential
+        '''
+
+        self.new_credential.save_credentials()
+        Credentials.find_by_account_name("Twitter")
+        
+        
+if __name__ == '__main__':
+    unittest.main()
