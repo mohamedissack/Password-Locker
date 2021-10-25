0db3d9e84f45a414828085cd3a7333bec59371ef
mohamedissack <mohamed.abdi@student.moringashool.com>
2021-10-25 07:13:31 +0300

    add def save credential

--- run.py
+++ run.py
@@ -45,3 +45,9 @@ def create_credential(user_name,account_name,password):
     letters = string.ascii_lowercase
     password_generated = ''.join(random.choice(letters) for i in range(length))
     return password_generated
+    
+    def save_credentials(credential):
+    '''
+    Function that saves new credential
+    '''
+    Credentials.save_credentials(credential)
