3060afb074ed14ed0ea64c5851c9bae8682d3071
mohamedissack <mohamed.abdi@student.moringashool.com>
2021-10-25 06:41:41 +0300

    add class credentials

--- credentials.py
+++ credentials.py
@@ -2,3 +2,10 @@ from user import User
 import random #import random variable generator
 import string  #import string constants
 import pyperclip
+
+class Credentials:
+    '''
+    Class to create  account credentials, generate new passwords and save user information
+    '''
+    
+    credential_list = []
