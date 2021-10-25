41b61413a2832f5dd4b6b1747f3c84d47000c0ea
mohamedissack <mohamed.abdi@student.moringashool.com>
2021-10-25 07:04:15 +0300

    add def create user

--- run.py
+++ run.py
@@ -4,3 +4,10 @@ from user import User
 from credentials import Credentials
 import random #import random variable generator
 import string  #import string constants
+
+def create_user(user_name,password):
+    '''
+    Function to create new user
+    '''
+    new_user = User(user_name,password)
+    return new_user
